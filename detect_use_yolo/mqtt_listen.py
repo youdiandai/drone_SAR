e629aa0106761a326aa7f19b5d23d56faffedc34
常鑫 <1561753947@qq.com>
2024-10-12 22:21:19 +0800

    edit mqtt_listen.py

--- detect_use_yolo/mqtt_listen.py
+++ detect_use_yolo/mqtt_listen.py
@@ -1,36 +1,25 @@
 import paho.mqtt.client as mqtt
-# Topic:thing/product/{device_sn}/osd
-# MQTT服务器地址和端口
-device_sn = "7CTDM3800B883B"
-MQTT_BROKER = "localhost"
-MQTT_PORT = 1883
-MQTT_TOPIC = f"thing/product/{device_sn}/osd"
-
-# MQTT用户名和密码
-MQTT_USERNAME = "admin"
-MQTT_PASSWORD = "admin123"
-
-# MQTT消息回调函数
-# def on_message(client, userdata, message):
-#     print(f"Received message '{str(message.payload)}' on topic '{message.topic}' with QoS {message.qos}")
+import json
 
 def on_message(client, userdata, message):
-    print(f"{message.payload}")
+    try:
+        # 将收到的消息转换为 JSON 对象
+        payload_json = message.payload.decode('utf-8')
+        payload_data = json.loads(payload_json)
+        print(payload_data)
+    except Exception as e:
+        print(f"Error decoding JSON: {e}")
 
-# 创建MQTT客户端实例
-client = mqtt.Client(protocol=mqtt.MQTTv5)  # 使用 MQTT v5 协议
+def subscribe_to_topic(device_sn):
+    MQTT_TOPIC = f"thing/product/{device_sn}/osd"
+    client = mqtt.Client(protocol=mqtt.MQTTv5)  # 使用 MQTT v5 协议
+    client.username_pw_set("admin", "admin123")
+    client.on_message = on_message
+    client.connect("localhost", 1883, 60)
+    client.subscribe(MQTT_TOPIC)
+    client.loop_forever()
 
-# 设置用户名和密码
-client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
 
-# 指定消息回调函数
-client.on_message = on_message
-
-# 连接到MQTT服务器
-client.connect(MQTT_BROKER, MQTT_PORT)
-
-# 订阅主题
-client.subscribe(MQTT_TOPIC)
-
-# 阻塞调用，直到客户端断开连接
-client.loop_forever()
+if __name__ == "__main__":
+    # 调用函数并传递设备序列号
+    subscribe_to_topic("7CTDM3800B883B")
